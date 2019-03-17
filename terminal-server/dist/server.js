1b4cb2fb4ac8a5598438c8625fb4e488940a42cc
Lucas Neiva <lucasneiva07@gmail.com>
2019-03-17 20:27:01 -0300

    finished the backend of the terminal portion. need to fix error on the client

--- terminal-server/dist/server.js
+++ terminal-server/dist/server.js
@@ -1,13 +1,13 @@
 "use strict";
 exports.__esModule = true;
 var express = require("express");
+var expressWs = require("express-ws");
 var dotenv = require("dotenv");
 var pty = require('node-pty');
 dotenv.config();
 var port = process.env.PORT;
-var app = express();
-require('express-ws')(app);
-var terminals = new Object();
+var app = expressWs(express()).app;
+var terminals = {}, logs = {};
 app.get('/', function (req, res) { return res.send('Hello World'); });
 app.use(function (req, res, next) {
     res.header('Access-Control-allow-Origin', '*')
@@ -17,8 +17,8 @@ app.use(function (req, res, next) {
 app.post('/terminals', function (req, res) {
     var cols = parseInt(req.query.cols, 10);
     var rows = parseInt(req.query.rows, 10);
-    var platform = process.platform === 'win32' ? 'cmd.exe' : 'bash';
-    var term = pty.spawn(platform, [], {
+    var shell = process.platform === 'win32' ? 'cmd.exe' : 'bash';
+    var term = pty.spawn(shell, [], {
         name: 'xterm-color',
         cols: cols,
         rows: rows,
@@ -26,9 +26,39 @@ app.post('/terminals', function (req, res) {
         env: process.env
     });
     console.log("created a terminal with PID: " + term.pid);
+    terminals[term.pid] = term;
+    term.on('data', function (data) {
+        logs[term.pid] += data;
+    });
+    res.send(term.pid.toString());
+    res.end();
 });
 app.post('/terminals/:pid/size', function (req, res) {
-    var pid = req.params.pid, _a = req.query, cols = _a.cols, rows = _a.rows, term = [pid];
+    var pid = req.params.pid, _a = req.query, cols = _a.cols, rows = _a.rows, term = terminals[pid];
+    term.resize(cols, rows);
+    console.log('Resized terminal ' + pid + ' to ' + cols + ' cols and ' + rows + ' rows.');
+    res.end();
+});
+app.ws('/tereminals/:pid', function (ws, req) {
+    var term = terminals[parseInt(req.params.pid, 10)];
+    !term ? ws.send('Terminal was not created') : console.log("Terminal connected on " + term.pid);
+    ws.send(logs[term.pid]);
+    term.on('data', function (data) {
+        try {
+            ws.send(data);
+        }
+        catch (_a) {
+        }
+    });
+    ws.on('message', function (msg) {
+        term.write(msg);
+    });
+    ws.on('close', function () {
+        term.kill();
+        console.log("Terminal " + term.pid + " closed");
+        delete terminals[term.pid];
+        delete logs[term.pid];
+    });
 });
 app.listen(port, function () {
     console.log("Server has been started on port " + port);
--- terminal-server/src/server.ts
+++ terminal-server/src/server.ts
@@ -1,14 +1,16 @@
 import {Request, Response, NextFunction} from 'express'
 import express = require('express');
+import expressWs = require('express-ws');
 import dotenv = require('dotenv');
+import { ITerminal } from 'node-pty/src/interfaces'
+import { WithWebsocketMethod } from 'express-ws';
 const pty = require('node-pty')
 dotenv.config();
 const port = process.env.PORT;
+const {app} = expressWs(express());
 
-const app : express.Application = express();
-require('express-ws')(app)
-
-const terminals : object = new Object()
+const terminals : {[index: number] : ITerminal} = {},
+      logs      : any                           = {}
 
 app.get('/', (req: Request,res: Response) => res.send('Hello World'));
 
@@ -22,9 +24,9 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 app.post('/terminals', (req, res) => {
     const cols = parseInt(req.query.cols, 10);
     const rows = parseInt(req.query.rows, 10);
-    const platform = process.platform === 'win32' ? 'cmd.exe' : 'bash'
+    const shell = process.platform === 'win32' ? 'cmd.exe' : 'bash'
 
-    const term = pty.spawn(platform,[], {
+    const term : ITerminal = pty.spawn(shell,[], {
         name: 'xterm-color',
         cols,
         rows,
@@ -32,14 +34,52 @@ app.post('/terminals', (req, res) => {
         env: process.env
     })
     console.log(`created a terminal with PID: ${term.pid}`)
-    // terminals[] = term;
+    terminals[term.pid] = term;
+    term.on('data', data=> {
+        logs[term.pid] += data;
+    })
+    res.send(term.pid.toString());
+    res.end();
     
 })
 
 app.post('/terminals/:pid/size', (req,res) => {
     const {pid}     = req.params,
         {cols,rows} = req.query,
-        term = [pid];
+        term = terminals[pid];
+
+        term.resize(cols, rows);
+        console.log('Resized terminal ' + pid + ' to ' + cols + ' cols and ' + rows + ' rows.');
+        res.end();
+})
+
+app.ws('/tereminals/:pid', (ws, req) => {
+    let term = terminals[parseInt(req.params.pid, 10)];
+
+    !term ? ws.send('Terminal was not created') : console.log(`Terminal connected on ${term.pid}`);
+
+    ws.send(logs[term.pid]);
+
+    term.on('data', data=> {
+        try {
+            ws.send(data);
+        }
+        catch{
+
+        }
+    })
+
+    ws.on('message', (msg:string) => {
+        term.write(msg)
+    })
+    ws.on('close', () => {
+        term.kill();
+        console.log(`Terminal ${term.pid} closed`);
+
+        delete terminals[term.pid];
+        delete logs[term.pid]
+    })
+
 })
 
 app.listen(port, () => {
